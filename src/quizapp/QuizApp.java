df9e8c6f87d7a01ff127853365082d0d202fee0c
calde <calde@CalebsLaptop>
2018-05-30 14:58:16 +1200

    Created the GUI, time to start on the coding

---
+++ src/quizapp/QuizApp.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package quizapp;
+
+/**
+ *
+ * @author calde
+ */
+public class QuizApp {
+
+    /**
+     * @param args the command line arguments
+     */
+public static void main( String args[] )
+	{
+		// Create an instance of the gui application
+		    
+    javax.swing.JFrame window = new javax.swing.JFrame("QuizApp");
+    window.getContentPane().add(new QuizAppGUI());
+    window.setVisible(true);  
+    window.pack(); 
+        window.setDefaultCloseOperation(javax.swing.JFrame.EXIT_ON_CLOSE);
+	}
+
+    
+}
